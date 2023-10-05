228b3053533fbebe8d659bbb99d967f6cfe97a41
PavlenkoIGOR <illusiopard@gmail.com>
2023-10-06 00:44:40 +0300

    addTextBox

--- watcher/MainWindow.xaml.cs
+++ watcher/MainWindow.xaml.cs
@@ -569,11 +569,11 @@ namespace watcher
             #region  создание textBox'ов для Grid в stackLayout
             TextBox textBoxSP1 = new TextBox() { MinHeight = 18.9, HorizontalAlignment = HorizontalAlignment.Stretch, Text = "", BorderBrush = Brushes.Black, BorderThickness = new Thickness(1,1,1,1), FontSize = 10, HorizontalContentAlignment = HorizontalAlignment.Left };            
             TextBox textBoxSP2 = new TextBox() { MinHeight = 18.9, HorizontalAlignment = HorizontalAlignment.Stretch, Text = "", BorderBrush = Brushes.Black, BorderThickness = new Thickness(1,1,1,1), FontSize = 10, HorizontalContentAlignment = HorizontalAlignment.Center};
-            //textBoxSP1.KeyDown += myTextBox_KeyDown;
-            textBoxSP1.AddHandler(TextBox.KeyDownEvent, new KeyEventHandler(myTextBox_KeyDown));
+            textBoxSP1.KeyDown += OnTextBoxKeyDown;
+            textBoxSP1.AddHandler(TextBox.KeyDownEvent, new KeyEventHandler(OnTextBoxKeyDown));
             textBoxSP1.TextWrapping = TextWrapping.Wrap; 
             textBoxSP1.AcceptsReturn = false;
-            textBoxSP2.AddHandler(TextBox.KeyDownEvent, new KeyEventHandler(myTextBox_KeyDown));
+            textBoxSP2.AddHandler(TextBox.KeyDownEvent, new KeyEventHandler(OnTextBoxKeyDown));
             textBoxSP2.TextWrapping = TextWrapping.Wrap;
             textBoxSP2.AcceptsReturn = false;
             Grid.SetColumn(textBoxSP1, 0);
@@ -648,6 +648,33 @@ namespace watcher
             sheetsAndSheet.Children.Add(tSheetsNumber);
 
             return sheetsAndSheet;
+        }
+
+        ///
+        ///доюавление textbox
+        ///
+        private void OnTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Grid grid = new Grid() { };
+                TextBox txt1 = new TextBox() { Name = "toolsCell", FontSize = 10, Height = 18.9, HorizontalAlignment = HorizontalAlignment.Stretch, Margin = new Thickness(0, 0, 0, 0), BorderBrush = Brushes.Black, BorderThickness = new Thickness(1, 1, 1, 1), HorizontalContentAlignment = HorizontalAlignment.Left };
+                txt1.AddHandler(TextBox.KeyDownEvent, new KeyEventHandler(myTextBox_KeyDown));
+                TextBox txt2 = new TextBox() { Name = "quantityCell", FontSize = 10, Height = 18.9, HorizontalAlignment = HorizontalAlignment.Stretch, Margin = new Thickness(0, 0, 0, 0), BorderBrush = Brushes.Black, BorderThickness = new Thickness(1, 1, 1, 1), HorizontalContentAlignment = HorizontalAlignment.Center };
+                txt2.AddHandler(TextBox.KeyDownEvent, new KeyEventHandler(myTextBox_KeyDown));
+
+                grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(18.9) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(158.7) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(37.0) });
+                grid.Children.Add(txt1);
+                grid.Children.Add(txt2);
+                Grid.SetColumn(txt2, 1);
+
+                
+                Grid parentGrid = (sender as TextBox).Parent as Grid;
+                StackPanel stPanel = parentGrid.Parent as StackPanel;
+                stPanel.Children.Add(grid);
+            }
         }
     }
 }
